48fdb223dd91fa59baefdf813281f6e05ac37526
JanEricLink <jan@designwerkstatt-trossingen.de>
2025-10-30 17:21:43 +0100

    Assignment 5 login and sign in process added

--- Assignment 5/server.js
+++ Assignment 5/server.js
@@ -7,6 +7,65 @@ const port = 3000;
 app.use(express.static("public"))
 
 // Routes
+app.get('/token', (req, res) => {
+    const token = req.headers['authorization'];
+    if (token) {
+        res.json({ valid: true });
+    } else {
+        res.status(401).json({ valid: false });
+    }
+});
+
+app.get('/login', (req, res) => {
+    const username = req.query.username;
+    const password = req.query.password;
+
+    // Beispiel: einfache Überprüfung (nicht für Produktion geeignet)
+    if (username === 'admin' && password === '1234') {
+        res.send('Login erfolgreich!');
+    } else {
+        res.send('Falscher Benutzername oder Passwort.');
+    }
+});
+
+app.get('/createuser', (req, res) => {
+    const username = req.query.username;
+    const firstname = req.query.firstname;
+    const lastname = req.query.lastname;
+    const email = req.query.email;
+    const password = req.query.password;
+    const userExists = Users.some(user => user.username === username);
+    if (userExists) {
+        res.send('Benutzername existiert bereits.');
+    } else {
+        Users.push(new User(username, firstname, lastname, email, password));
+        res.sendFile(path.join(__dirname, 'public', 'acc_created.html'));
+    }
+});
+
+// Code
+    //Classes
+    class User {
+        constructor(username, firstname, lastname, email, password) {
+            this.username = username;
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.email = email;
+            this.password = password;
+            this.todos = [];
+        }
+    }
+
+    class Todo {
+        constructor(title, description) {
+            this.title = title;
+            this.description = description;
+            this.completed = false;
+        }
+    }
+
+    //Variables
+    let Users = [new User('admin', 'admin', 'admin', 'admin@admin.de', '1234')];
 
 
 // Start Server
